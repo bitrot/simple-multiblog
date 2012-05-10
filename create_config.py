b9ad342fe3f974cb9c831096992faefcc5072afe
Ryan Macy <me@rmacy.us>
2012-05-10 18:15:44 -0500

    updated model, updating config
    Signed-off-by: Ryan Macy <me@rmacy.us>

--- create_config.py
+++ create_config.py
@@ -1,5 +1,4 @@
 from werkzeug.security import generate_password_hash
-from flaskext.sqlalchemy import SQLAlchemy
 
 def input_with_default(prompt, default):
     x = raw_input("%s (Default %s) "%(prompt, default))
@@ -11,21 +10,30 @@ print "Generating a Simple config file. Please answer some questions:"
 
 with open("settings.py", "w") as fd:
     fd.write("# -*- coding: utf-8 -*-\n\n")
-    fd.write("POSTS_PER_PAGE = %s\n"%input_with_default("Posts per page", 5))
-#    fd.write("ADMIN_USERNAME = '%s'\n"%input_with_default("Admin username","admin"))
-#    fd.write("ADMIN_PASSWORD = '%s'\n"%generate_password_hash(input_with_default("Admin password","password")) )
-    fd.write("ANALYTICS_ID = '%s'\n"%input_with_default("Google analytics ID",""))
-    sqlalchemy_uri = input_with_default("Database URI","sqlite:///simple.db")
-    fd.write('SQLALCHEMY_DATABASE_URI = "%s"\n'%sqlalchemy_uri)
-#    fd.write("GITHUB_USERNAME = '%s'\n"%input_with_default("Github Username", ""))
-#    fd.write("CONTACT_EMAIL = '%s'\n"%input_with_default("Contact Email", ""))
-    fd.write("BLOG_TITLE = '%s'\n"%input_with_default("Blog title", ""))
-    fd.write("BLOG_TAGLINE = '%s'\n"%input_with_default("Blog tagline", ""))
-    fd.write("BLOG_URL = '%s'\n"%input_with_default("Blog URL",""))
-    fd.flush()
 
-import model
-db = SQLAlchemy(sqlalchemy_uri)
+    fd.write("POSTS_PER_PAGE = %s\n"%input_with_default("Posts per page", 5))
+
+    admin_username = input_with_default("Admin username","admin")
+
+    admin_password = generate_password_hash(input_with_default("Admin password","password"))
+
+    fd.write("ANALYTICS_ID = '%s'\n"%input_with_default("Google analytics ID",""))
+
+    db_uri = input_with_default("Database URI","sqlite:///simple.db")
+
+    fd.write('SQLALCHEMY_DATABASE_URI = "%s"\n'%sqlalchemy_uri)
+
+    github_username = input_with_default("Github Username", "")
+
+    admin_email = input_with_default("Contact Email", "")
+
+    fd.write("BLOG_TITLE = '%s'\n"%input_with_default("Blog title", ""))
+
+    fd.write("BLOG_TAGLINE = '%s'\n"%input_with_default("Blog tagline", ""))
+
+    fd.write("BLOG_URL = '%s'\n"%input_with_default("Blog URL",""))
+
+    fd.flush()
 
 username = input_with_default("Admin username","admin")
 password = generate_password_hash(input_with_default("Admin password","password"))
@@ -38,3 +46,5 @@ db.session.commit()
 print "Created!"
 
 raw_input()
+
+# Base.metadata.create_all(Engine)
--- model.py
+++ model.py
@@ -1,38 +1,74 @@
-# setup SQLAlchemy models
-# TODO: use declarative base
-class Post(db.Model):
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text, Sequence, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+import datetime
+import json
+import markdown
+
+Base = declarative_base()
+
+class Post(Base):
     __tablename__ = "posts"
 
-    id           = db.Column(db.Integer, primary_key=True)
-    title        = db.Column(db.String())
-    author       = db.Column(db.ForeignKey(User.id)) # this should be a FK
-    slug         = db.Column(db.String(), unique=True)
-    text         = db.Column(db.String(), default="")
-    draft        = db.Column(db.Boolean(), index=True, default=True)
-    views        = db.Column(db.Integer(), default=0)
-    created_at   = db.Column(db.DateTime, default=datetime.datetime.utcnow(), index=True)
-    updated_at   = db.Column(db.DateTime, default=datetime.datetime.utcnow(), onupdate=datetime.datetime.utcnow())
+    id           = Column(Integer(11), Sequence('post_id_sequence'), primary_key = True, nullable = False)
+    title        = Column(String(255))
+    author       = Column(ForeignKey(User.id), nullable = False)
+    slug         = Column(String(255), unique = True)
+    text         = Column(String(255), default = "")
+    draft        = Column(Boolean, index = True, default = True)
+    views        = Column(Integer(11), default = 0)
+    created_at   = Column(DateTime, default=datetime.datetime.utcnow(), index = True)
+    updated_at   = Column(DateTime, default=datetime.datetime.utcnow(), onupdate = datetime.datetime.utcnow())
+
+    def __init__(Self, *args, **kwargs):
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+
+    def __unicode__(self):
+        return "%s %s %s" % (self.title, self.text, self.created_at)
+
+    public_field = ('id', 'title', 'author', 'slug', 'text', 'draft', 'views', 'created_at', 'updated_at')
+
+    def get_public(self):
+        data = {}
+        for field in self.public_field:
+            data[field] = getattr(self, field, '')
+        return data
+
+    def get_json(self):
+        return json.dumps(self.get_public())
 
     def render_content(self):
         return markdown.Markdown(extensions=['fenced_code'], output_format="html5", safe_mode=True).convert(self.text)
 
-class User(db.Model):
+class User(Base):
     __tablename__ = "users"
 
-    id           = db.Column(db.Integer, primary_key=True)
-    username     = db.Column(db.String(), index=True)
-    password     = db.Column(db.String())
-    email        = db.Column(db.String())
-    github       = db.Column(db.String())
-    bio          = db.Column(db.Text())
-    created_at   = db.Column(db.DateTime, default=datetime.datetime.utcnow(), index=True)
-    updated_at   = db.Column(db.DateTime, default=datetime.datetime.utcnow(), onupdate=datetime.datetime.utcnow())
+    id           = Column(Integer(11), Sequence('user_id_sequence'), primary_key = True, nullable = False)
+    username     = Column(String(255), index = True)
+    password     = Column(String(255))
+    email        = Column(String(255))
+    github       = Column(String(255))
+    bio          = Column(Text)
+    created_at   = Column(DateTime, default=datetime.datetime.utcnow(), index = True)
+    updated_at   = Column(DateTime, default=datetime.datetime.utcnow(), onupdate=datetime.datetime.utcnow())
+
+    relationship(Post)
+
+    def __init__(self, *args, **kwargs):
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
 
     def __unicode__(self):
-        return "%s %s" % (self.id, self.username)
+        return "%s %s %s %s" % (self.id, self.username, self.email, self.github)
 
-# create if not exists -- SQLAlchemy
-try:
-    db.create_all()
-except Exception:
-    app.logger.error(format_exc())
+    public_field = ('id', 'username', 'email', 'github', 'bio', 'created_at')
+
+    def get_public(self):
+        data = {}
+        for field in self.public_field:
+            data[field] = getattr(self, field, '')
+        return data
+
+    def get_json(self):
+        return json.dumps(self.get_public())
