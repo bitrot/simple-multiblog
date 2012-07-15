da999f0d65054bd801cbd079b0dd5e0acdb3a30d
Eric Haughee <ehaughee@gmail.com>
2012-07-14 20:41:36 -0400

    Epic editor works with draft mode's auto save

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,48 +1,54 @@
 // 100% stolen from yiransheng/simple
-$.fn.autogrow = function(options) {
 
-    this.filter('textarea').each(function() {
+//$.fn.autogrow = function(options) {
+//
+//    this.filter('textarea').each(function() {
+//
+//        var $this       = $(this),
+//                minHeight   = $this.height(),
+//                lineHeight  = $this.css('lineHeight');
+//
+//        var shadow = $('<div></div>').css({
+//            position:   'absolute',
+//            top:        -10000,
+//            left:       -10000,
+//            width:      $(this).width(),
+//            fontSize:   $this.css('fontSize'),
+//            fontFamily: $this.css('fontFamily'),
+//            lineHeight: $this.css('lineHeight'),
+//            resize:     'none'
+//        }).appendTo(document.body);
+//
+//        var update = function() {
+//
+//            var val = this.value.replace(/</g, '&lt;')
+//                    .replace(/>/g, '&gt;')
+//                    .replace(/&/g, '&amp;')
+//                    .replace(/\n/g, '<br/>');
+//
+//            shadow.html(val);
+//            $(this).css('height', Math.max(shadow.height() + 60, minHeight));
+//        }
+//
+//        $(this).change(update).keyup(update).keydown(update);
+//
+//        update.apply(this);
+//
+//    });
+//
+//    return this;
+//
+//};
 
-        var $this       = $(this),
-                minHeight   = $this.height(),
-                lineHeight  = $this.css('lineHeight');
 
-        var shadow = $('<div></div>').css({
-            position:   'absolute',
-            top:        -10000,
-            left:       -10000,
-            width:      $(this).width(),
-            fontSize:   $this.css('fontSize'),
-            fontFamily: $this.css('fontFamily'),
-            lineHeight: $this.css('lineHeight'),
-            resize:     'none'
-        }).appendTo(document.body);
-
-        var update = function() {
-
-            var val = this.value.replace(/</g, '&lt;')
-                    .replace(/>/g, '&gt;')
-                    .replace(/&/g, '&amp;')
-                    .replace(/\n/g, '<br/>');
-
-            shadow.html(val);
-            $(this).css('height', Math.max(shadow.height() + 60, minHeight));
-        }
-
-        $(this).change(update).keyup(update).keydown(update);
-
-        update.apply(this);
-
-    });
-
-    return this;
-
-};
-
-
-function issueSaveAjax(id, redirect){
+function issueSaveAjax(id, redirect, elem){
     var ptitle   = $("#post_title").val();
-    var pcontent = $("#post_content").val();
+    var pcontent = '';
+    if (elem)
+        pcontent = elem.innerText;
+    else
+        pcontent = $("#post_content").val();
+
     var req = $.ajax({
         type: "POST",
         url:"/save/"+id,
@@ -57,7 +63,7 @@ function issueSaveAjax(id, redirect){
         }
     })
 }
-
-$(function() {
-    $('textarea').autogrow();
-});
+//
+//$(function() {
+//    $('textarea').autogrow();
+//});
